11f1c3d4978b720095cc935811646a4f09d3049c
danny-oli <danny.oli91@gmail.com>
2021-09-22 22:39:41 -0300

    Relationship created between Product and Ingredient on ProductSchema; Creating JwtAuthGuard on ingredientes.controller

--- src/ingredients/ingredients.controller.ts
+++ src/ingredients/ingredients.controller.ts
@@ -6,30 +6,35 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { IngredientsService } from './ingredients.service';
 import { CreateIngredientDto } from './dto/create-ingredient.dto';
 import { UpdateIngredientDto } from './dto/update-ingredient.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('ingredients')
 export class IngredientsController {
-  constructor(private readonly ingredientsService: IngredientsService) {}
+  constructor(private readonly ingredientsService: IngredientsService) { }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createIngredientDto: CreateIngredientDto) {
     return this.ingredientsService.create(createIngredientDto);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.ingredientsService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.ingredientsService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -38,6 +43,7 @@ export class IngredientsController {
     return this.ingredientsService.updateOne(id, updateIngredientDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.ingredientsService.deleteOne(id);
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,23 +1,23 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { Ingredient } from 'src/ingredients/entities/ingredient.entity';
+
+import * as mongoose from 'mongoose'
 
 export type ProductDocument = Product & Document;
 
 @Schema()
 export class Product {
-  @Prop()
+  @Prop({ required: true })
   name: string;
 
   @Prop()
   image: string;
 
-  @Prop()
-  value: string;
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Ingredient' })
+  ingredients: Ingredient
 
-  @Prop()
-  ingredients: string;
-
-  @Prop()
+  @Prop({ required: true })
   quantity: number;
 }
 
